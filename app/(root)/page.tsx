72c2bd7853368f361e955865b1dfbc306a61f138
Phuong To <phuong.toduc@nastechglobal.com>
2023-09-26 00:05:21 +0700

    Init project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+export default function Home() {
+  return (
+    <div className="p-4">
+        <Button size="icon" variant="destructive">Click me</Button>
+    </div>
+  )
+}
